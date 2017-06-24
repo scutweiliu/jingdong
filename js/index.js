b4f492053871ec1a746a5b17df06d38810590832
weiliu <weiliu@example.com>
2017-06-24 17:55:30 +0800

    version0.1

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+	var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween:0,
+        autoplay: 1500,
+        loop: true
+    });
+}
