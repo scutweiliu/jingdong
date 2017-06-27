9aa15893ec8d4c22ec7c4dac1d49ab8f49a354cf
weiliu <weiliu@example.com>
2017-06-27 22:39:09 +0800

    version0.2

--- js/index.js
+++ js/index.js
@@ -7,4 +7,27 @@ window.onload=function(){
         autoplay: 1500,
         loop: true
     });
+    
+    //倒计时
+    setmiaosha();
+    setInterval(function() {
+    	setmiaosha();
+    }, 1000);
+    function setmiaosha(){
+    	var nowdate = new Date();
+    	var nowtime = nowdate.getTime();
+    	nowdate.setDate(28);
+    	nowdate.setHours(20);
+    	nowdate.setMinutes(0);
+    	nowdate.setSeconds(0);
+    	var settime = nowdate.getTime();
+    	var s = parseInt(((settime - nowtime) / 1000) % 60);
+    	var m = parseInt(((settime - nowtime) / 1000) / 60 % 60);
+    	var h = parseInt(((settime - nowtime) / 1000) / 60 / 60);
+    	var items = document.querySelectorAll(".product_time>.jd_title>a>i");
+//  	console.log(items)
+    	items[0].innerHTML = h;
+    	items[1].innerHTML = m;
+    	items[2].innerHTML = s;
+    }
 }
